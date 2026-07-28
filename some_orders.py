3027d4f83b819ba4a54156d8f00f852bd0ba47d2
youzi-baby <chenfeixd@gmail.com>
2026-07-28 10:55:02 +0800

    Add some orders script

---
+++ some_orders.py
@@ -0,0 +1,3 @@
+[Environment]::SetEnvironmentVariable("ADARUBRIC_BASE_URL", "http://localhost:8000/v1", "User")
+[Environment]::SetEnvironmentVariable("ADARUBRIC_API_KEY", "你的_api_key", "User")
+[Environment]::SetEnvironmentVariable("ADARUBRIC_MODEL", "你的本地模型名", "User")
